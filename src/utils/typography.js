cd2963e8f4850c16b6c734016f1b079be29e9c3d
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-12-11 16:58:22 +0700

    Add hot reload for font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -22,4 +22,9 @@ const typography = new Typography({
   })
 })
 
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles()
+}
+
 export default typography
