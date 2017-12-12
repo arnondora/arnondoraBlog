07d657a2aebac4bf1fdda5128f5ec4818e8b1102
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-12-12 20:52:05 +0700

    Add Link Utils

---
+++ src/utils/link.js
@@ -0,0 +1,15 @@
+export function getFacebookShareLink (link) {
+  return 'https://www.facebook.com/sharer/sharer.php?u=' + link
+}
+
+export function getTwitterShareLink (link) {
+  return 'https://twitter.com/home?status=' + process.env.SHARE_TWITTER_LINK + link
+}
+
+export function getGooglePlusShareLink (link) {
+  return 'https://plus.google.com/share?url=' + link
+}
+
+export function convertLinkFromSlug (slug) {
+  return process.env.APP_URL + "/" + slug
+}
