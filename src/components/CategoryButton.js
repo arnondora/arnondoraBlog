8f66afc792e30908d239b16a6b03fe008e780655
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-12-13 22:36:44 +0700

    Add Thumbnail to Category Button

--- src/components/CategoryButton.js
+++ src/components/CategoryButton.js
@@ -5,23 +5,30 @@ import Link from 'gatsby-link'
 import colours from '../utils/colours'
 import animator from "./animator.module.css";
 
+
 const Container = styled.div`
-  background-color: ${props => props.hover ? colours.primaryColour : 'white'};
+  background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${props => props.thumbnail}) ${props => props.hover ? colours.primaryColour : 'white'};
+  background-position:center;
+  background-repeat: no-repeat;
   color: white;
   box-shadow: 0 2px 5px 0 rgba(0,0,0,0.05);
   height: 300px;
-  width: 100%;
+  width:30%;
+  min-width: 200px;
   max-height: 300px;
+  margin: 10px;
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
   overflow: hidden;
-  flex: 1;
-  margin-left: 50px;
-  :first-child{
-    margin-left: 0px;
+
+  @media (max-width: 693px)
+  {
+    width:100%;
+    margin-left: 0;
   }
 `
+
 
 const ContentWrapper = styled.div`
   display: flex;
@@ -29,16 +36,22 @@ const ContentWrapper = styled.div`
   min-width: 200px;
   width:95%;
   margin-left: 10px;
-  transform: translateY(85px);
+  transform: translateY(90px);
+  @media (max-width: 693px)
+  {
+    transform: translateY(60px);
+  }
 `
 
 const CatName = styled(Link)`
   font-size: 36px;
-  color: ${colours.textHeading};
+  line-height: 1.2;
+  color: white;
   margin-bottom: 20px;
 `
 const CatDescription = styled.p`
-  color: ${colours.textHeading};
+  color: white;
+  width:100%;
   visibility: hidden;
   font-size: 18px;
   margin-bottom: 33px;
@@ -49,7 +62,7 @@ export default class CategoryButton extends React.Component {
     var slug = "/" + this.props.slug
 
     return (
-      <Container className = {animator.parent}>
+      <Container thumbnail={this.props.thumbnail} className = {animator.parent}>
         <ContentWrapper className = {animator.fadein}>
             <CatName className = {animator.text} to = {slug}>{this.props.name}</CatName>
             <CatDescription className = {animator.text} to = {slug}>{this.props.description}</CatDescription>
