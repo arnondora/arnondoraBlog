6665407109ab720ad442290bec782182e87abf09
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-12-16 20:00:55 +0700

    Add RecommendStory Feature

---
+++ src/components/RecommendStory.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+
+import colours from '../utils/colours'
+
+const Container =  styled.div`
+  display: flex;
+  flex-direction:row;
+  flex-wrap:nowrap;
+
+  @media (max-width: 768px) {
+    flex-direction:column;
+    flex-wrap:wrap;
+  }
+`
+
+const StoryWrapper = styled.div`
+  background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${props => props.thumbnail}), ${colours.primaryColour};
+  background-position:center;
+  background-repeat: no-repeat;
+  padding: 0.5em;
+  display: flex;
+  flex-grow: 1;
+  height:200px;
+  justify-content: center;
+  align-items: center;
+`
+
+const Story = styled(Link)`
+  font-size: 1.2em;
+  color:white;
+  text-align: center;
+  vertical-align: middle;
+  text-align: center;
+  margin:0;
+`
+
+export default class RecommendStory extends React.Component {
+  render () {
+    return (
+      <Container>
+        {
+          this.props.stories.map((story,index) => {
+            console.log(story)
+            return (
+              <StoryWrapper key={story.node.fields.slug} thumbnail={story.node.frontmatter.landscapeThumbnail}>
+                <Story to={story.node.fields.slug}>
+                  {story.node.frontmatter.title}
+                </Story>
+              </StoryWrapper>
+            )
+          })
+        }
+      </Container>
+    )
+  }
+}
