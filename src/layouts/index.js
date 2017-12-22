fc8a5ac76c403e19bd9da8cf01c01f2a85d25d5f
Arnon Puitrakul <peter_arnon1@icloud.com>
2017-12-22 17:54:39 +0700

    Add SEO for all pages

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,12 +3,21 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
+import siteLogo from '../assets/favicon.png'
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title="Hello World - Mad Programmer Diary"
       meta={[
         { name: 'description', content: 'The offical personal blog of @arnondora' },
+        {property: "og:title", "content" : 'Hello World - Mad Programmer Diary'},
+        {property: "og:description", "content" : 'The offical personal blog of @arnondora'},
+        {property: "og:locale", "content" : "th_TH"},
+        {property: "og:type", "content": "blog"},
+        {property: "og:url", "content": "https://staging.arnondora.in.th"},
+        {property: "og:image:secure_url", "content": siteLogo},
+        {property: "og:site_name", "content": 'Hello World - Mad Programmer Diary'},
       ]}
     >
       <html lang="th"/>
