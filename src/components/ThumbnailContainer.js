7c472e996a489b8162c2a263a64706cc61e1e028
Arnon Puitrakul <peter_arnon1@icloud.com>
2018-03-16 00:44:46 +0700

    Remove unused lodash functions

--- src/components/ThumbnailContainer.js
+++ src/components/ThumbnailContainer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
-import { get, isEmpty } from 'lodash'
+import { isEmpty } from 'lodash'
 
 import colours from '../utils/colours'
 
--- src/templates/blog-post-full-width.js
+++ src/templates/blog-post-full-width.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { get, take, isEmpty } from 'lodash'
+import { isEmpty } from 'lodash'
 
 import colours from '../utils/colours'
 
