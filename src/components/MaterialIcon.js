612346ebb352c44ba86d175fadefa9eda4917e31
Arnon Puitrakul <peter_arnon1@icloud.com>
2018-05-09 21:34:35 +0700

    Add Google Material Component

---
+++ src/components/MaterialIcon.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+import {get} from 'lodash'
+
+const Icon = styled.i`
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: ${props => props.size};
+  color: ${props => props.colour? props.colour: 'white'};
+  display: inline-block;
+  line-height: 1;
+  text-transform: none;
+  letter-spacing: normal;
+  word-wrap: normal;
+  white-space: nowrap;
+  direction: ltr;
+
+  /* Support for all WebKit browsers. */
+  -webkit-font-smoothing: antialiased;
+  /* Support for Safari and Chrome. */
+  text-rendering: optimizeLegibility;
+
+  /* Support for Firefox. */
+  -moz-osx-font-smoothing: grayscale;
+
+  /* Support for IE. */
+  font-feature-settings: 'liga';
+`
+
+export default class MaterialIcon extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Icon size={this.props.size}>{this.props.iconName}</Icon>
+      </React.Fragment>
+    )
+  }
+}
