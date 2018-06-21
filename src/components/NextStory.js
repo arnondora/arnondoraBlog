fc774a80a3372c2b3b5a61932782ca11ba1fe65a
Arnon Puitrakul <peter_arnon1@icloud.com>
2018-06-22 00:38:22 +0700

    Add Dark Theme to NextStory

--- src/components/NextStory.js
+++ src/components/NextStory.js
@@ -4,9 +4,9 @@ import Link from 'gatsby-link'
 import color from 'color'
 
 const SuperWrapper = styled.div`
-  background-color: ${props => props.theme.secondaryBackground};
-  border-top: 1px solid ${props => props.theme.secondaryBorder};
-  border-bottom: ${props => props.hasRelated? '1px solid ' + props.theme.secondaryBorder : 'none'};
+  background-color: ${props => props.isNight? props.theme.night_lightBackground : props.theme.secondaryBackground};
+  border-top: 1px solid ${props => props.isNight? props.theme.night_secondaryBorder : props.theme.secondaryBorder};
+  border-bottom: ${props => props.hasRelated ? '1px solid ' + props.isNight ? props.theme.night_secondaryBorder : props.theme.secondaryBorder : 'none'};
   padding-top: 35px;
   padding-bottom: 35px;
 `
@@ -46,6 +46,14 @@ const PreviousStoryContainer = StoryContainer.extend`
   float: left;
   padding-left: 20px;
 
+  ${Label} {
+    color: ${props => props.isNight ? props.theme.night_text_normal : "#666666"};
+  }
+
+  & > a, & > a:visited, & > a:hover {
+    color: ${props => props.isNight ? props.theme.night_text_light : props.theme.primaryColour};
+  }
+
   @media (max-width: 768px) {
     padding-left:0;
   }
@@ -55,13 +63,20 @@ const NextStoryContainer = StoryContainer.extend`
   float: right;
   padding-right: 20px;
 
+  ${Label} {
+    color: ${props => props.isNight ? props.theme.night_text_normal : "#666666"};
+  }
+
+  & > a, & > a:visited, & > a:hover {
+    color: ${props => props.isNight ? props.theme.night_text_light : props.theme.primaryColour};
+  }
+
   @media (max-width: 768px) {
     padding-right:0;
   }
 `
 
 const Label = styled.span`
-  color: ${props => props.theme.textSecondary};
   font-size: 1.2em;
   font-weight: 300;
   margin: 0;
@@ -78,7 +93,6 @@ const Label = styled.span`
 `
 
 const Heading = styled(Link)`
-  color: ${props => props.theme.primaryColour};
   font-size: 1.4em;
   font-weight: 700;
   margin: 10px 0 0 0;
@@ -104,11 +118,11 @@ const ContentWrapper = styled.div`
 export default class NextStory extends React.Component {
   render() {
     return (
-        <SuperWrapper hasRelated={this.props.hasRelated}>
+        <SuperWrapper hasRelated={this.props.hasRelated} isNight={this.props.isNight}>
           <Container>
             {
               this.props.next != false ?
-              <NextStoryContainer>
+              <NextStoryContainer isNight={this.props.isNight}>
                     <Label>Previous</Label>
                     <Heading to={this.props.next.fields.slug}>← {this.props.next.frontmatter.title}</Heading>
               </NextStoryContainer>
@@ -118,7 +132,7 @@ export default class NextStory extends React.Component {
 
             {
               this.props.prev != false ?
-              <PreviousStoryContainer>
+              <PreviousStoryContainer isNight={this.props.isNight}>
                     <Label>Next</Label>
                     <Heading to = {this.props.prev.fields.slug}>{this.props.prev.frontmatter.title} →</Heading>
               </PreviousStoryContainer>
