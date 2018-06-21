d83a2975c59878d647629c1407d87e2ae9df34ed
Arnon Puitrakul <peter_arnon1@icloud.com>
2018-06-22 00:38:36 +0700

    Add new Dark Theme Colours

--- src/utils/colours.js
+++ src/utils/colours.js
@@ -5,10 +5,15 @@ const colours = {
   textSecondary: "#666666",
   textLowProfile: "#ADADAD",
   secondaryBackground: "#FAFAFA",
+  defaultBackground: "F5F5F5",
   darkBackground: "#F9F9F9",
   secondaryBorder: "#E0E0E0",
 
   night_darkBackground: "#22313a",
+  night_lightBackground: "#2b3942",
+  night_text_normal: "#b4bfc3",
+  night_text_light: "#edeff0",
+  night_secondaryBorder: "#171f25",
 
   facebook: "#3b5998",
   twitter: "#55acee",
