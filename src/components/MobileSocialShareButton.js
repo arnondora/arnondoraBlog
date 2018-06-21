08e01130cff4c946f0f3ff0c23044b2d839fe9f9
Arnon Puitrakul <peter_arnon1@icloud.com>
2018-06-22 01:48:24 +0700

    Fix prop name to match with naming convention

--- src/components/MobileSocialShareButton.js
+++ src/components/MobileSocialShareButton.js
@@ -38,7 +38,7 @@ const IconLink = styled.a`
 
 const Icon = styled(FontAwesomeIcon)`
   align-self: center;
-  color: ${props => props.isNight? props.theme.night_text_normal : color(props.theme.textSecondary).lighten(0.2).string()};
+  color: ${props => props.isnight ? props.theme.night_text_normal : color(props.theme.textSecondary).lighten(0.2).string()};
 
   &:hover {
     color: ${props => props.name === "facebook" ? props.theme.facebook : props.name === "twitter" ? props.theme.twitter : props.theme.googlePlus}
@@ -52,9 +52,9 @@ export default class MobileSocialShareButton extends React.Component {
       <Container isNight = {this.props.isNight}>
           <Header>Share this article to social network?</Header>
           <ButtonGroup>
-            <IconLink href = {getFacebookShareLink(link)} rel="noopener" target="_blank" aria-label="Share to Facebook"><Icon icon={faFacebookF} isNight={this.props.isNight} name={"facebook"} size="2x"/></IconLink>
-            <IconLink href = {getTwitterShareLink(link)} rel="noopener" target="_blank" aria-label="Share to Twitter"><Icon icon={faTwitter} isNight={this.props.isNight} name={"twitter"} size="2x"/></IconLink>
-            <IconLink href = {getGooglePlusShareLink(link)} rel="noopener" target="_blank" aria-label="Share to Google Plus"><Icon icon={faGooglePlus} isNight={this.props.isNight} name={"google+"} size="2x"/></IconLink>
+            <IconLink href = {getFacebookShareLink(link)} rel="noopener" target="_blank" aria-label="Share to Facebook"><Icon icon={faFacebookF} isnight={this.props.isNight ? 1 : 0} name={"facebook"} size="2x"/></IconLink>
+            <IconLink href = {getTwitterShareLink(link)} rel="noopener" target="_blank" aria-label="Share to Twitter"><Icon icon={faTwitter} isnight={this.props.isNight ? 1 : 0} name={"twitter"} size="2x"/></IconLink>
+            <IconLink href = {getGooglePlusShareLink(link)} rel="noopener" target="_blank" aria-label="Share to Google Plus"><Icon icon={faGooglePlus} isnight={this.props.isNight ? 1 : 0} name={"google+"} size="2x"/></IconLink>
           </ButtonGroup>
       </Container>
     )
