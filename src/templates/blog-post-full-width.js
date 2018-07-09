0374ed84259bcb37fcf6a4d58b731c773b762c24
Arnon Puitrakul <peter_arnon1@icloud.com>
2018-07-09 22:49:45 +0700

    Refactor content container to seperated file

--- src/templates/blog-post-full-width.js
+++ src/templates/blog-post-full-width.js
@@ -14,6 +14,7 @@ import MobileSocialShareButton from '../components/MobileSocialShareButton'
 import AppearanceController from '../components/AppearanceController'
 import StickyMobileShare from '../components/StickyMobileShare'
 import Footer from '../components/Footer'
+import ArticleWrapper from '../components/BlogContent'
 
 import './blog-post-full-width.css' /* Import Reader Style */
 
@@ -51,75 +52,6 @@ const ContentWrapper = styled.div`
   @media (max-width: 768px) {
     width: 90%;
     margin-top: 25px;
-  }
-`
-const ArticleWrapper = styled.div`
-  margin-top: 20px;
-
-  @media (max-width: 768px) {
-    margin-top: 0;
-  }
-
-  & > *,
-  & > blockquote > p,
-  & > ul > li,
-  & > ul > li > strong,
-  & > ol > li,
-  & > ol > li > strong {
-    color: ${props =>
-      props.isNight ? props.theme.night_text_light : props.theme.textHeading};
-  }
-
-  pre > code {
-    background-color: ${props =>
-      props.isNight
-        ? props.theme.night_lightBackground
-        : props.theme.secondaryBackground} !important;
-    white-space: pre-wrap;
-    display: block;
-    padding: 10px 15px 10px 15px;
-    color: ${props =>
-      props.isNight ? props.theme.night_text_light : '#222222'};
-    border: 1px solid
-      ${props =>
-        props.isNight ? props.theme.night_secondaryBorder : '#e6e6e6'};
-    border-radius: 8px;
-  }
-
-  & > h1 {
-    font-size: ${props => props.scale + 2.6058 + 'rem'};
-  }
-
-  & > h2 {
-    font-size: ${props => props.scale + 2.00448 + 'rem'};
-  }
-
-  & > h3 {
-    font-size: ${props => props.scale + 1.6704 + 'rem'};
-  }
-
-  & > h4 {
-    font-size: ${props => props.scale + 1.392 + 'rem'};
-  }
-
-  & > p,
-  li {
-    font-size: ${props => props.scale + 1.1 + 'rem'};
-  }
-
-  & > p:first-child::first-letter {
-    color: #2096f3;
-    font-size: ${props => props.scale + 3.1 + 'rem'};
-    line-height: 40px;
-    float: left;
-    padding-top: ${props => props.scale * 10 + 12 + 'px'};
-    padding-right: 10px;
-    padding-left: 3px;
-  }
-
-  & > p > img {
-    width: 100%;
-    margin: 0 auto;
   }
 `
 
